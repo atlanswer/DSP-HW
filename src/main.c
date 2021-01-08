029b14f92b44aa3c4827bda8dacd98cc832941b0
Atlanswer <atlanswer@gmail.com>
2021-01-09 01:40:11 +0800

    Mailbox test.

--- src/main.c
+++ src/main.c
@@ -2,8 +2,8 @@
  * @file main.c
  * @author Atlanswer (atlanswer@gmail.com)
  * @brief Task 6 main logic.
- * @version 0.1
- * @date 2021-01-07
+ * @version 0.2
+ * @date 2021-01-08
  * 
  * @copyright Copyright (c) 2021
  * 
@@ -11,16 +11,16 @@
 
 // DSP/BIOS API
 #include <std.h>
-#include <stdlib.h>
 #include <log.h>
+#include <sio.h>
 #include <mbx.h>
 #include <tsk.h>
 // Custom headers
 #include "DSP-HWcfg.h"
 
 // Number of messages
-#define NUMMSGS     3
-#define TIMEOUT     10
+const int NUM_MSGS = 3;
+const int TIMEOUT = 1;
 
 /**
  * @brief Message object
@@ -31,56 +31,47 @@ typedef struct MsgObj {
     Int     id;
     // Message value.
     Char    val;
-} MsgObj, *Msg;
+} MsgObj, *pMsg;
 
-Void reader(Void);
-Void writer(Arg id_arg);
+Void readRandChar(Arg id_arg);
+Void genRandChar(Arg id_arg);
 
 // Main logic
 Void main(void) {
-    LOG_printf(&trace, "Hello world!");
     /* fall into DSP/BIOS idle loop */
     return;
 }
 
-Void reader(Void)
-{
-    MsgObj      msg;
-    Int         i;
-
-    for (i=0; ;i++) {
-
-        /* wait for mailbox to be posted by writer() */
-        if (MBX_pend(&mbx, &msg, TIMEOUT) == 0) {
-            LOG_printf(&trace, "timeout expired for MBX_pend()");
+Void readRandChar(Arg id_arg) {
+    MsgObj msg;
+    Int id = ArgToInt(id_arg);
+    LOG_printf(&randCharLog, "[readRandChar](%d) spawned.", id);
+    while (TRUE) {
+        // Wait for mailbox
+        if (MBX_pend(&mbx, &msg, TIMEOUT) == FALSE) {
+            LOG_printf(&randCharLog, "[readRandChar](%d) Mailbox timeout.", id);
             break;
         }
-
-        /* print value */
-        LOG_printf(&trace, "read '%c' from (%d).", msg.val, msg.id);
+        LOG_printf(&randCharLog,
+                   "[readRandChar](%d) Read '%c' form (%d).", id, msg.val, msg.id);
     }
-    LOG_printf(&trace, "reader done.");
+    LOG_printf(&randCharLog, "[readRandChar](%d) died.");
 }
 
-Void writer(Arg id_arg)
-{
-    MsgObj      msg;
-    Int         i;
-    Int id =    ArgToInt (id_arg);
-
-    for (i=0; i < NUMMSGS; i++) {
-        /* fill in value */
+Void genRandChar(Arg id_arg) {
+    MsgObj msg;
+    Int cMsg;
+    Int id = ArgToInt(id_arg);
+    LOG_printf(&randCharLog, "[genRandChar](%d) spawned.", id);
+    for (cMsg = 0; cMsg < NUM_MSGS; ++cMsg) {
         msg.id = id;
-        msg.val = i % NUMMSGS + (Int)('a');
-
-        /* enqueue message */
-        MBX_post(&mbx, &msg, TIMEOUT);
-       
-        LOG_printf(&trace, "(%d) writing '%c' ...", id, (Int)msg.val);
-
-        /* what happens if you call TSK_yield() here? */
-        /* TSK_yield(); */
+        msg.val = 'a' + cMsg;
+        if (MBX_post(&mbx, &msg, TIMEOUT) == FALSE) {
+            LOG_printf(&randCharLog, "[genRandChar](%d) Mailbox full.", id);
+            continue;
+        }
+        LOG_printf(&randCharLog,
+                   "[genRandChar](%d) is writing '%c'.", id, msg.val);
     }
-
-    LOG_printf(&trace, "writer (%d) done.", id);
+    LOG_printf(&randCharLog, "[genRandChar](%d) died.", id);
 }
